b65bdef80206637961655e45a6040a346d8bbd21
Alexander <workmail.4alex@gmail.com>
2021-05-22 23:42:49 +0300

    Change #_12

--- application/server/main.py
+++ application/server/main.py
@@ -33,14 +33,14 @@ async def predict_api(file: UploadFile = File(...)):
         content = await file.read()
         await out_file.write(content) 
     IMG_SIZE = 150
-    image = plt.imread("application/images/" + file.filename)
+    image = cv.imread("application/images/" + file.filename, cv.IMREAD_GRAYSCALE)
     #image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     image_new = cv.resize(image,(IMG_SIZE, IMG_SIZE))
     image_new = image_new.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
     #image = read_imagefile(file_location)
     result = model.predict_classes(image_new)
 
-    return {"class":result[0]}
+    return {"class": result[0][0]}
 
 
 
