a4721317265a8fdc2556dfd66f2507a1d2a4d2fb
Alexander <workmail.4alex@gmail.com>
2021-05-23 00:01:38 +0300

    Change #_12

--- application/server/main.py
+++ application/server/main.py
@@ -40,7 +40,7 @@ async def predict_api(file: UploadFile = File(...)):
     #image = read_imagefile(file_location)
     result = model.predict_classes(image_new)
 
-    return {"path":os.path.exists("application/images/" + file.filename), "file":file.filename}#{"class": result[0][0]}
+    return {"path_":os.path.exists("application/images/" + file.filename), "file":file.filename, "class": result[0][0]}#{"class": result[0][0]}
 
 
 
